18974552ff8cf4f61a08f9dac851cc4c22aaef7b
Sean Madondo <seanmadondo@yahoo.com>
2022-01-26 21:12:34 +1300

    setting up bp app

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>About Us</title>
+        <meta name="description" content="CMDT About Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default About;
